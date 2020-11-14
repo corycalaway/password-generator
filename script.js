6a3ae730690ec9ad523138876415b80cfe97df26
corycalaway <cory.c.calaway@gmail.com>
2020-11-14 14:11:59 -0700

    initial code starting testing

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
 // Assignment code here
+// Series of prompts for password criteria slect options include
+// choose length of password. selct a number 8-128. Make reselect if anything that is not 8-128 is selected
+// Arrays
+var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
+// password function
+var generatePassword = function () {
 
 
+// length function
+//var passLength = function () {
+    var promptLength = window.prompt('How long would you like your password to be? Please enter a number from 8 to 128.');
+    
+    //Users response invalid
+    if (promptLength <=7 || promptLength >=129) {
+        window.alert('Please select a valid number.')
+        return passLength();
+    }
+
+
+//};
+};
+/*
+length 8-128
+special characters : lowercase
+uppercase
+numeric
+and/or special characters
+generate password matching criteria at least one of each
+diplayed password on page or in alert
+*/
+
+// password object
+//var passwordInfo = {
+ //   length: promptLength();
+//}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,7 +43,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
