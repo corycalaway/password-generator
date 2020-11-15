a1b6880a073aae75f06b7595005f4fa6fd841fe5
corycalaway <cory.c.calaway@gmail.com>
2020-11-14 19:43:46 -0700

    starting to put together password info and lowercase confirm

--- script.js
+++ script.js
@@ -7,34 +7,49 @@ var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
 var specialCharArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=','?', '/', '<', '>'];
 var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 // length function
-var passLength = function () {
+var generatePassword = function () {
     var promptLength = window.prompt('How long would you like your password to be? Please enter a number from 8 to 128.');
     
     //Users response invalid
     if (promptLength <=7 || promptLength >=129) {
         window.alert('Please select a valid number.')
-        return passLength();
+        return generatePassword();
+    } else {
+    promptLength;
     }
+    console.log(promptLength)
 
+    var lowerCaseConfirm = window.confirm("Would you like to include lowercase characters in your password?");
 
+    console.log(lowerCaseConfirm)
+    if (lowerCaseConfirm === true) {
+        passwordInfo.lower += 1;
+    } else {
+        passwordInfo.lower += 0;
+    }
+    
 };
+
+// option of characters confirm
+
+// Prompt asking to add lowerCaseArray
+
+
+// option of characters confirm
+
+//var charOptions = function () {
+  //  passwordInfo.lowerCaseSelect();
+//};
+// Prompt asking to add upperCaseArray
+
+// Prompt asking to add specialCharArray
+//var lowerchar = confirm
+//if(lowerchar)
+//Result.push(lowercasearr)
+// Prompt asking to add numericArray
 
 // password function
-var generatePassword = function () {
-return passLength();
-};
-// length function
-var passLength = function () {
-    var promptLength = window.prompt('How long would you like your password to be? Please enter a number from 8 to 128.');
-    
-    //Users response invalid
-    if (promptLength <=7 || promptLength >=129) {
-        window.alert('Please select a valid number.')
-        return passLength();
-    }
 
-
-};
 
 /*
 length 8-128
@@ -47,9 +62,13 @@ diplayed password on page or in alert
 */
 
 // password object
-//var passwordInfo = {
- //   length: promptLength();
-//}
+var passwordInfo = {
+//length: 0,
+//upperCaseArr: 
+lower: 0,
+//specialCharArr:
+//numericArr:
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -59,7 +78,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
